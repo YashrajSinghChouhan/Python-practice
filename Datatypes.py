e4cfedd97920e6616353bf30fce97cfdc86324d1
Yashraj Singh Chouhan <yashrajsingh@Yashrajs-MacBook-Pro.local>
2024-01-10 23:55:40 +0530

    adding content to datatypes.py again

---
+++ Datatypes.py
@@ -0,0 +1,22 @@
+a = 1
+b = 'a'
+c = "hello"
+d = '''
+
+Multi line text
+
+'''
+e = True
+print(a,b,c,d,e)
+print('''
+--------------
+''')
+
+f = ("tuple",11,2.4,False)
+g = ["list",4,5.0,True]
+h = {"key":"value","name":"yashraj"}
+i = {"it","is","a","a","set"}
+print(f,g,h,i)
+print('''
+---------------
+''')
